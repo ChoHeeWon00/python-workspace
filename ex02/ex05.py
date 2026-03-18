d132e4e00b02017d6bdc79ff2259e7b07ca4c1dc
choheewon <choheewon@hanmail.net>
2026-03-18 12:13:51 +0900

    02

--- ex02/ex05.py
+++ ex02/ex05.py
@@ -3,7 +3,16 @@ file = open("C:\\ibm3-workspace/python-workspace/test2.txt", "a")
 file.write("ssssss")
 file.close()
 '''
+'''
 file = open("C:\\ibm3-workspace/python-workspace/test2.txt", "r")
 str_ = file.read()
 file.close()
 print( str_ )
+'''
+file = open("C:\\ibm3-workspace/python-workspace/test.jpg", "rb")
+image = file.read()
+file.close()
+
+file = open("C:\\ibm3-workspace/python-workspace/복사이미지.jpg", "wb")
+file.write(image)
+file.close()
